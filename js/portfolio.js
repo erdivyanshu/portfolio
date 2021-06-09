60c28a772c2622c5bafbd01345f929e932f198c9
Divyanshu Yadav <77803707+erdivyanshu@users.noreply.github.com>
2021-06-09 11:04:08 +0530

    portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing",{
+    strings:["Web Developer", "Designer", "FreeLancer", "Engineer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+var typed = new Typed(".typing-2",{
+    strings:["Web Developer .", "Designer .", "FreeLancer .", "Engineer ."],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
